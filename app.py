88cb7cc35a8beea98f51a4e55516911694f21787
riddhidoshi771 <riddhidoshi771@gmail.com>
2021-02-07 19:04:45 +0530

    Added ProtgSQL URL

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -9,7 +11,7 @@ from resources.item import Item,ItemList
 from resources.store import Store,StoreList
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///data.db' #can be mysql or any other sql
+app.config['SQLALCHEMY_DATABASE_URI']=osenviron.get('DATABASE_URL','sqlite:///data.db') #can be mysql or any other sql
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 app.secret_key = 'jose'
 api = Api(app)
